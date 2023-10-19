03f178f44f525a9675904e3b07ab7a36717d6746
Ubuntu <ubuntu@ip-172-31-1-101.ec2.internal>
2023-10-19 11:19:54 +0000

    commit

---
+++ a.rb
@@ -0,0 +1,12 @@
+require 'bcrypt'
+
+signup_password = BCrypt::Password.create("my")
+puts signup_password
+
+user = User.find_by(uid: params[:uid])
+
+login_password = BCrypt::Password.new(user.pass)
+
+if login_password == 'my' #params[:pass]
+    puts "OK"
+end
